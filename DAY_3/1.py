b0da5af97f602583650da8ebd936f0a301955a4b
Neil Patel <84774538+neil8501@users.noreply.github.com>
2021-05-31 17:45:04 +0530

    Add files via upload

---
+++ DAY_3/1.py
@@ -0,0 +1,3 @@
+list = [1 , 2 , 3 , 4 , 5]
+average= sum(list) / len(list)
+print("Average of 5 numbers is :" , average)
---
+++ DAY_3/9.py
@@ -0,0 +1,9 @@
+n1=int(input("enter a no. :"))
+
+if n1<100:
+    if n1%2==0:
+        print("the number is even :")
+    else:
+        print("the number is odd .")
+else:
+    print("the number is not less than 100 .")
