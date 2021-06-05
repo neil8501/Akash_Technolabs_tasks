bc0c93a8528a30da980447ed0aad5925d43cdba2
Neil Patel <84774538+neil8501@users.noreply.github.com>
2021-06-05 15:12:12 +0530

    Add files via upload

---
+++ DAY_8/Theme/myapp/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def homepageview(request):
+    return render(request,'home.html')
+
+
+def aboutpageview(request):
+    return render(request,'about.html')
+
+
+def contactpageview(request):
+    return render(request,'contact.html')
+
